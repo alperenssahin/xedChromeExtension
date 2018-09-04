feda998a6be995ffb0232889b48d6d67f07b51f3
lebigmac <allperensahin@gmail.com>
2018-09-04 14:36:17 +0300

    init user interface

--- pop-upJs/style.js
+++ pop-upJs/style.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
     $('#text.monitor').text('Select crawl type');
     $(document).click(function (ev) {
@@ -6,24 +7,25 @@ $(document).ready(function () {
             let id = setup.operation.style(dom);
             setup.operation.finish();
             if (id === 'mapping') {
-               setup.next('Start');
+                $('#next.setup').removeClass('blocked');
+                setup.next('Start');
             }
             if (id === 'scraping') {
                 $('#reference-recurrent.setup').removeClass('blocked');
             }
         }
-        if(dom.hasClass('ref-rec')){
+        if (dom.hasClass('ref-rec')) {
             let id = setup.ref_rec.style(dom);
             setup.ref_rec.finish();
-            if(id==='reference'){
+            if (id === 'reference') {
                 $('#next.setup').removeClass('blocked');
-                 setup.next('Next');
+                setup.next('Next');
 
-                 //....
+                //....
                 //next data
             }
-            if(id==='recurrent'){
-                 $('#next.setup').removeClass('blocked');
+            if (id === 'recurrent') {
+                $('#next.setup').removeClass('blocked');
                 setup.next('Start');
 
                 //next data
@@ -31,6 +33,15 @@ $(document).ready(function () {
             }
         }
     });
+    $('#next.setup').click(function () {
+        if ($('#next.setup').attr('data-button') === 'next') {
+            page.deactivate($('#container.setup'));
+            page.activate($('#container.attr-ref'));
+        } else {
+
+        }
+    });
+    events.run();
 
 });
 var setup = {
@@ -58,8 +69,8 @@ var setup = {
             $('.setup.info-rr').slideUp(200);
         }
     },
-    ref_rec:{
-        style:function (dom) {
+    ref_rec: {
+        style: function (dom) {
             $('.setup.info-rr').slideUp(200);
             let id = dom.attr('id');
             console.log(id);
@@ -78,7 +89,65 @@ var setup = {
             $('#next.setup').addClass('blocked');
         }
     },
-    next:function (text) {
+    next: function (text) {
         $('#next.setup').text(text);
+        if (text === 'Next') {
+            $('#next.setup').attr('data-button', 'next');
+        }
+        if (text === 'Start') {
+            $('#next.setup').attr('data-button', 'start');
+        }
     }
 };
+
+var events = {
+    run: function () {
+        this.backStep();
+        this.settings();
+    },
+    backStep: function () {
+        $('span').click(function () {
+            if ($(this).hasClass('attr-ref')) {
+                page.deactivate($('#container.attr-ref'));
+                page.activate($('#container.setup'));
+                //todo:destroy request
+            }
+        });
+    },
+    settings: function () {
+        $('span').click(function () {
+
+            if ($(this).hasClass('settings')) {
+
+                let setting = $('#container.settings');
+                if (setting.hasClass('inactive')) {
+                    page.lastActive = $('#container.active');
+                    page.deactivate(page.lastActive);
+                    page.activate(setting);
+                    $('#settings.button').addClass('opened');
+                } else {
+                    page.activate(page.lastActive);
+                    page.deactivate(setting);
+                    $('#settings.button').removeClass('opened');
+
+                }
+
+            }
+        });
+    }
+
+};
+var page = {
+    activate: function (dom) {
+        dom.removeClass('inactive');
+        dom.addClass('active');
+    }
+    ,
+    deactivate: function (dom) {
+        dom.removeClass('active');
+        dom.addClass('inactive');
+    },
+    lastActive: null
+    ,
+};
+
--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
-
 'use strict';
 //define port http://127.0.0.1:8003
 // $('#get-cat').click(function () {
 $(document).ready(function () {
+    XedCrawler.init();
     $.post('http://127.0.0.1:8003/xedEx/', {setup: 1}, function (data) {
         console.log(data);
         var obj = JSON.parse(data);
@@ -29,20 +29,19 @@ $(document).ready(function () {
         });
 
     });
-    //todo: server isimlerini 1 değişkene bağla, option ile kontrol  edilebilir
     $('#get-rules').click(function () {
         chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {greeting: "rules"}, function (response) {
                 // console.log('trig');trig
-                if (response.error === 0){
+                if (response.error === 0) {
                     //data işlenebilir
                     // console.log('heel');
                     let myJSONdata = JSON.stringify(response.data);
-                    $.post('http://127.0.0.1:8003/rulePort/',{data:myJSONdata},function (data) {
-                       console.log(data);
+                    $.post('http://127.0.0.1:8003/rulePort/', {data: myJSONdata}, function (data) {
+                        console.log(data);
                     });
-                }else{
-                    console.log('error no :'+response.error);
+                } else {
+                    console.log('error no :' + response.error);
                     return 0;
                     //hata nuramarasını basar
                 }
@@ -50,6 +49,82 @@ $(document).ready(function () {
         });
     });
 });
+var XedCrawler = {
+    init: function () {
+        let login = this.checkLogIn();
+        if (login == undefined || login == false) {
+            this.config.login = false;
+        } else {
+            this.config.login = true;
+        }
+        this.config.setup();
+    },
+    checkLogIn: function () {
+        chrome.storage.local.get(['logIn'], function (result) {
+            return result.logIn;
+
+        });
+    },
+    setLogInStatus: function (status) {
+        chrome.storage.local.set({logIn: status}, function () {
+            console.log('LogIn value is set to ' + status);
+        });
+    },
+    logOut: function () {
+    },
+    //configuration general...
+    config: {
+        login: false,
+        server: 'http://127.0.0.1:8003',
+        setup: function () {
+            XedCrawler.authentication.config();
+        },
+        user: null,
+    },
+
+    authentication: {
+        //configuration about authentication...
+        config: function () {
+            if (XedCrawler.config.login) {
+                let dom = $('#container.general');
+                page.deactivate($('#container-up.authentication'));
+                page.activate(dom);
+                $('#container-up.header').removeClass('inactive');
+            }
+            else {
+                $('#send.authentication').click(function () {
+                    //todo:connection request, control response
+                    let response = XedCrawler.authentication.request();
+
+                    if (response) {
+                        page.deactivate($('#container-up.authentication'));
+                        page.activate($('#container.general'));
+                        $('#container-up.header').removeClass('inactive');
+                        XedCrawler.setLogInStatus(true);
+
+                    } else {
+                        XedCrawler.setLogInStatus(false);
+                        //todo:affiche error message
+
+                    }
+                });
+            }
+
+        },
+        request: function () {
+            let url = XedCrawler.config.server + '/login';
+            console.log('preparing to request-->' + url);
+            let data = {}
+            $.post(url, data, function (data) {
+                let res = JSON.parse(data);
+                //ctrl...
+                return true //todo:create user object on server.
+                //return false...
+
+            });
+        },
+    }
+};
 
 
 
